9bdf9d3d6f7e38f4de9883680ceaa3a0cccad395
Usersive <lawalusersive@gmail.com>
2025-03-12 18:32:10 +0100

    head

--- quizproject/settings.py
+++ quizproject/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY =('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'djangoreactquizapi.onrender.com']
+ALLOWED_HOSTS = ['djangoreactquizapi.onrender.com']
 
 
 # Application definition
